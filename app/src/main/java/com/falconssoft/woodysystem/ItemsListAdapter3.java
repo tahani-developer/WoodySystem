058d6aa11d6735f4bcf3a4596a8db75c78d18835
sundos <sundos.tamimi20@gmail.com>
2019-10-29 12:16:48 +0200

    loading order report

---
+++ app/src/main/java/com/falconssoft/woodysystem/ItemsListAdapter3.java
@@ -0,0 +1,84 @@
+package com.falconssoft.woodysystem;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.falconssoft.woodysystem.models.BundleInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemsListAdapter3 extends BaseAdapter {
+
+    private Context context;
+    private List<BundleInfo> mOriginalValues;
+    private static List<BundleInfo> itemsList;
+    private static List<BundleInfo> selectedBundles ;
+
+    public ItemsListAdapter3(Context context, List<BundleInfo> itemsList) {
+        this.context = context;
+        this.mOriginalValues = itemsList;
+        this.itemsList = itemsList;
+        selectedBundles = new ArrayList<>();
+    }
+
+    public ItemsListAdapter3() {
+
+    }
+
+    public void setItemsList(List<BundleInfo> itemsList) {
+        this.itemsList = itemsList;
+    }
+
+    @Override
+    public int getCount() {
+        return itemsList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    private class ViewHolder {
+        TextView th, w, l, grade, pcs, bundle, location, area;
+    }
+
+    @Override
+    public View getView(final int i, View view, ViewGroup viewGroup) {
+
+        final ViewHolder holder = new ViewHolder();
+        view = View.inflate(context, R.layout.item_row3, null);
+
+        holder.th = (TextView) view.findViewById(R.id.th);
+        holder.w = (TextView) view.findViewById(R.id.w);
+        holder.l = (TextView) view.findViewById(R.id.l);
+        holder.grade = (TextView) view.findViewById(R.id.grade);
+        holder.pcs = (TextView) view.findViewById(R.id.pcs);
+        holder.bundle = (TextView) view.findViewById(R.id.bundle);
+        holder.location = (TextView) view.findViewById(R.id.location);
+        holder.area = (TextView) view.findViewById(R.id.area);
+
+        holder.th.setText("" + itemsList.get(i).getThickness());
+        holder.w.setText("" + itemsList.get(i).getWidth());
+        holder.l.setText("" + itemsList.get(i).getLength());
+        holder.grade.setText("" + itemsList.get(i).getGrade());
+        holder.pcs.setText("" + itemsList.get(i).getNoOfPieces());
+        holder.bundle.setText("" + itemsList.get(i).getBundleNo());
+        holder.location.setText("" + itemsList.get(i).getLocation());
+        holder.area.setText("" + itemsList.get(i).getArea());
+
+        return view;
+    }
+
+}
