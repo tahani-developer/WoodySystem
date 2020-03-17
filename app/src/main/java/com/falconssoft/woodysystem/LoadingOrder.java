b6e4e396b6344c4f26628a79946b3968982f5865
sundos <sundos.tamimi20@gmail.com>
2020-03-17 13:16:23 +0200

    loading order report - delete order

--- app/src/main/java/com/falconssoft/woodysystem/LoadingOrder.java
+++ app/src/main/java/com/falconssoft/woodysystem/LoadingOrder.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
