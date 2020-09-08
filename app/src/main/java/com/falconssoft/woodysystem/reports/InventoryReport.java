821cb37360fdc3d9f3556a0a37c6d29dbae7ca16
user54 <Rawan>
2020-09-08 15:41:01 +0300

    rawan(printer A5 in Xsls )

--- app/src/main/java/com/falconssoft/woodysystem/reports/InventoryReport.java
+++ app/src/main/java/com/falconssoft/woodysystem/reports/InventoryReport.java
@@ -1008,7 +1008,7 @@ public class InventoryReport extends AppCompatActivity implements AdapterView.On
 
                 if (!dateFrom.getText().toString().equals("") && !dateTo.getText().toString().equals("")) {
                     filters();
-                }
+                }  
             }
         };
         return date;
